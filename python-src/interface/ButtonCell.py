ad48ef7c8f2d4c6fce7625098d83b67bbe201895
akhtyamovpavel <akhtyamovpavel@gmail.com>
2015-03-29 21:25:52 +0300

    Second part of big commit

--- python-src/interface/ButtonCell.py
+++ python-src/interface/ButtonCell.py
@@ -18,14 +18,14 @@ class ButtonCell(QtGui.QToolButton):
     def __init__(self, language):
         super(ButtonCell, self).__init__()
         self.__language__ = language
-        self.clicked().connect(self.on_letter_clicked)
+        self.clicked.connect(self.on_letter_clicked)
 
         self.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
         self.create_menu()
 
 
     def create_menu(self):
-        self.__signal_mapper__ = QtCore.QSignalMapper(self)
+        self.signal_mapper = QtCore.QSignalMapper(self)
         self.__menu__ = QtGui.QMenu('Set of letters')
 
         self.__actions__ = list()
@@ -33,14 +33,19 @@ class ButtonCell(QtGui.QToolButton):
         current_language = self.__language__.get_list()
 
         for letter in current_language:
-            self.__actions__.append(QtGui.QAction(letter, self))
-            self.__menu__.addAction(self.__actions__[-1])
-            self.__signal_mapper__.setMapping(self.__actions__[-1], letter)
-            self.__actions__[-1].triggered.connect(self.__signal_mapper__.map)
+            action = QtGui.QAction(letter, self)
 
-        self.__signal_mapper__.mapped().connect(self.set_letter)
 
+            self.__menu__.addAction(action)
+            self.signal_mapper.setMapping(action, letter)
+
+            action.triggered.connect(self.signal_mapper.map)
+            self.__actions__.append(action)
+
+        self.signal_mapper.mapped[str].connect(self.set_letter)
+        self.setMenu(self.__menu__)
 
     def connect_to_panel(self, gb):
-        pass
+        self.on_letter_chosen.connect(gb.on_cell_chosen)
+        self.on_letter_pushed.connect(gb.on_cell_pushed)
 
--- python-src/interface/GraphicBoard.py
+++ python-src/interface/GraphicBoard.py
@@ -20,60 +20,64 @@ class GraphicBoard(QtGui.QWidget):
     give_up_first = QtCore.Signal()
     give_up_second = QtCore.Signal()
     
-    def __init__(self, width, height):
+    def __init__(self, width, height, parent):
         super(GraphicBoard, self).__init__()
 
         self.__width__ = width
-        self.__hegiht__ = height
+        self.__height__ = height
         self.__language__ = None
         lang_list = ['Russian', 'English']
-        game_lang = QtGui.QInputDialog.getItem(self, 'Choose language of the game', 'Выберите язык игры', lang_list)
+        game_lang, flag2 = QtGui.QInputDialog.getItem(self, 'Choose language of the game', 'Выберите язык игры', lang_list)
         if game_lang == 'Russian':
             self.__language__ = RussianLanguage()
         else:
             self.__language__ = EnglishLanguage()
 
-        self.__table_layout__ = QtGui.QGridLayout()
-        self.__buttons__ = list()
+        self.table_layout = QtGui.QGridLayout()
+        self.buttons = list()
 
         for i in range(height):
             tmp_button_list = list()
             for j in range(width):
-                tmp_button = ButtonCell(game_lang)
+                tmp_button = ButtonCell(self.__language__)
                 tmp_button.connect_to_panel(self)
-                self.__table_layout__.addWidget(tmp_button, i, j)
+                self.table_layout.addWidget(tmp_button, i, j)
                 tmp_button_list.append(tmp_button)
-            self.__buttons__.append(tmp_button_list)
+            self.buttons.append(tmp_button_list)
 
-        self.__game_panel__ = QtGui.QHBoxLayout()
-        self.__game_board_panel__ = QtGui.QVBoxLayout()
-        self.__game_board_panel__.addLayout(self.__table_layout__)
+        self.game_panel = QtGui.QHBoxLayout()
+        self.game_board_panel = QtGui.QVBoxLayout()
+        self.game_board_panel.addLayout(self.table_layout)
 
-        self.__word_panel__ = QtGui.QHBoxLayout()
-        self.__current_word__ = QtGui.QLabel('')
-        self.__commit_button__ = QtGui.QPushButton('Enter!')
-        self.__commit_give_up__ = QtGui.QPushButton('Give up')
-        self.__word_panel__.addWidget(self.__commit_give_up__)
-        self.__word_panel__.addWidget(self.__current_word__)
-        self.__word_panel__.addWidget(self.__commit_button__)
+        self.word_panel = QtGui.QHBoxLayout()
+        self.current_word = QtGui.QLabel('')
+        self.commit_button = QtGui.QPushButton('Enter!')
+        self.commit_give_up = QtGui.QPushButton('Give up')
+        self.word_panel.addWidget(self.commit_give_up)
+        self.word_panel.addWidget(self.current_word)
+        self.word_panel.addWidget(self.commit_button)
 
-        self.__game_board_panel__.addLayout(self.__word_panel__)
+        self.game_board_panel.addLayout(self.word_panel)
 
-        self.__first_player_score__ = QtGui.QLabel('0', self)
-        self.__first_player_words__ = QtGui.QListWidget(self)
-        self.__first_player_panel__ = QtGui.QVBoxLayout(self)
-        self.__first_player_panel__.addWidget(self.__first_player_score__)
-        self.__first_player_panel__.addWidget(self.__first_player_words__)
+        self.first_player_score = QtGui.QLabel('0', self)
+        self.first_player_words = QtGui.QListWidget(self)
+        self.first_player_panel = QtGui.QVBoxLayout()
+        self.first_player_panel.addWidget(self.first_player_score)
+        self.first_player_panel.addWidget(self.first_player_words)
 
-        self.__second_player_score__ = QtGui.QLabel('0', self)
-        self.__second_player_words__= QtGui.QListWidget(self)
-        self.__second_player_panel__ = QtGui.QListWidget(self)
-        self.__second_player_panel__.addWidget(self.__second_player_score__)
-        self.__second_player_panel__.addWidget(self.__second_player_words__)
+        self.second_player_score = QtGui.QLabel('0', self)
+        self.second_player_words = QtGui.QListWidget(self)
+        self.second_player_panel = QtGui.QVBoxLayout()
+        self.second_player_panel.addWidget(self.second_player_score)
+        self.second_player_panel.addWidget(self.second_player_words)
 
-        self.setLayout(self.__game_panel__)
+        self.game_panel.addLayout(self.first_player_panel)
+        self.game_panel.addLayout(self.game_board_panel)
+        self.game_panel.addLayout(self.second_player_panel)
 
-        players = QtGui.QInputDialog.getInt(self, 'Enter number of players', 'Введите число игроков', 1, 1, 2)
+        self.setLayout(self.game_panel)
+
+        players, flag = QtGui.QInputDialog.getInt(self, 'Enter number of players', 'Введите число игроков', 1, 1, 2)
         if players == 2:
             self.__game_manager__ = GameManager(self.__language__, width=width, height=height, players_number=players)
         else:
@@ -89,62 +93,119 @@ class GraphicBoard(QtGui.QWidget):
 
         word = self.__game_manager__.get_first_word()
         for i in range(width):
-            self.__buttons__[height//2][i].setText(word[i])
-            self.__buttons__[height//2][i].setMenu(None)
+            self.buttons[height//2][i].setText(word[i])
+            self.buttons[height//2][i].setMenu(None)
 
-        self.__commit_button__.clicked.connect(self.on_commit_button_clicked)
-        self.__commit_give_up__.clicked.connect(self.on_commit_give_up_clicked)
+        self.commit_button.clicked.connect(self.on_commit_button_clicked)
+        self.commit_give_up.clicked.connect(self.on_commit_give_up_clicked)
 
         self.__game_manager__.game_ended.connect(self.finish_game)
 
 
-        #self.quit.connect(self.resetField())
+        self.quit.connect(parent.reset_field)
 
 
     @QtCore.Slot()
     def on_cell_pushed(self):
-        pass
+        x_sender = 0
+        y_sender = 0
+        for i in range(self.__height__):
+            for j in range(self.__width__):
+                if self.sender() == self.buttons[i][j]:
+                    x_sender = i
+                    y_sender = j
+        send_letter = Coordinates(x_sender, y_sender)
+        if self.__game_manager__.get_current_player() == 1:
+            self.push_letter_first.emit(send_letter)
+        else:
+            self.push_letter_second.emit(send_letter)
 
     @QtCore.Slot(str)
     def on_cell_chosen(self, letter):
-        pass
+        x_sender = 0
+        y_sender = 0
+        for i in range(self.__height__):
+            for j in range(self.__width__):
+                if self.sender() == self.buttons[i][j]:
+                    x_sender = i
+                    y_sender = j
+
+        send_letter = CellLetter(x_sender, y_sender, letter)
+        if self.__game_manager__.get_current_player() == 1:
+            self.choose_letter_first.emit(send_letter)
+        else:
+            self.choose_letter_second.emit(send_letter)
 
     @QtCore.Slot(Coordinates)
     def after_cell_pushed(self, coordinates: Coordinates):
-        pass
+        self.current_word.setText(self.current_word.text() +
+                                      self.buttons[coordinates.x][coordinates.y].text())
 
     @QtCore.Slot(CellLetter)
     def after_cell_chosen(self, coordinates: CellLetter):
-        pass
+        self.buttons[coordinates.x][coordinates.y].setMenu(None)
+        self.buttons[coordinates.x][coordinates.y].setText(coordinates.letter)
 
 
     @QtCore.Slot(str)
     def after_word_committed(self, word):
-        pass
+        self.current_word.setText(None)
+        if self.sender() == self.__game_manager__.get_first_player():
+            self.first_player_words.addItem(word)
+            score = self.__game_manager__.get_first_player().get_score()
+            self.first_player_score.setText(str(score))
+        else:
+            self.second_player_words.addItem(word)
+            score = self.__game_manager__.get_second_player().get_score()
+            self.second_player_score.setText(str(score))
+        self.run_step()
+
 
     @QtCore.Slot(Coordinates)
     def on_player_reset_word(self, coordinates: Coordinates):
-        pass
+        self.buttons[coordinates.x][coordinates.y].createMenu()
+        self.buttons[coordinates.x][coordinates.y].setText('')
+        self.current_word.setText('')
 
     @QtCore.Slot()
     def on_commit_button_clicked(self):
-        pass
+        if self.__game_manager__.get_current_player() == 1:
+            self.commit_word_first.emit()
+        else:
+            self.commit_word_second.emit()
 
     @QtCore.Slot()
     def on_commit_give_up_clicked(self):
-        pass
+        if self.__game_manager__.get_current_player() == 1:
+            self.give_up_first.emit()
+        else:
+            self.give_up_second.emit()
 
     @QtCore.Slot(str)
     def finish_game(self, message):
-        pass
-    
-
+        box = QtGui.QMessageBox(self)
+        box.setText(message)
+        ok_button = box.addButton(QtGui.QMessageBox.Ok)
+        box.exec_()
+        if box.clickedButton() == ok_button:
+            self.quit.emit()
 
     def connect_to_players(self, player1: Player, player2: Player):
-        pass
+        self.choose_letter_first.connect(player1.on_letter_chosen)
+        self.push_letter_first.connect(player1.on_letter_pushed)
+
+        self.choose_letter_second.connect(player2.on_letter_chosen)
+        self.push_letter_second.connect(player2.on_letter_pushed)
+
+        self.commit_word_first.connect(player1.on_word_committed)
+        self.commit_word_second.connect(player2.on_word_committed)
+
+        self.give_up_first.connect(player1.send_end)
+        self.give_up_second.connect(player2.send_end)
+
 
     def run_step(self):
-        pass
+        self.__game_manager__.run_game()
 
 
 
