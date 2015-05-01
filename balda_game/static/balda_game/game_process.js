7f8cc930174f16a2c73fbce34f3b3dda71d51d25
akhtyamovpavel <akhtyamovpavel@gmail.com>
2015-05-02 01:30:04 +0300

    Fixed refresh error, added reset button(new) and pushing letters

--- balda_game/static/balda_game/game_process.js
+++ balda_game/static/balda_game/game_process.js
@@ -4,18 +4,42 @@
  */
 $(document).ready(function() {
 
-    var pinnedWeightLevel = 0;
-    var pinnedHeightLevel = 0;
+    var pinnedWeightLevel = -1;
+    var pinnedHeightLevel = -1;
     var isPinned = false;
 
     var game_id = $(location).attr('pathname').split('/')[2];
 
-    function getCellElement(x, y) {
-        if (x < 0 && x >= 5 || y < 0 && y >= 5) {
+
+    var currentWord = [];
+    var currentWeightLevelWord = [];
+    var currentHeightLevelWord = [];
+    var currentWordStr = "";
+
+    function resetWord() {
+        currentWord = [];
+        currentHeightLevelWord = [];
+        currentWeightLevelWord = [];
+        isPinned = 0;
+        pinnedWeightLevel = -1;
+        pinnedHeightLevel = -1;
+        currentWordStr = "";
+    }
+
+    function addLetter(heightLevel, widthLevel, letter) {
+        currentWord[currentWord.length] = letter.trim();
+        currentWeightLevelWord[currentWeightLevelWord.length] = widthLevel;
+        currentHeightLevelWord[currentHeightLevelWord.length] = heightLevel;
+        currentWordStr += letter.trim();
+        $($(".current_word")[0]).val(currentWordStr);
+    }
+
+    function getCellElement(currentHeight, currentWidth) {
+        if (currentHeight < 0 && currentHeight >= 5 || currentWidth < 0 && currentWidth >= 5) {
             return;
         }
         var table = $("#field_up");
-        return $($($($(table).children()[0]).children()[x]).children()[y]);
+        return $($($($(table).children()[0]).children()[currentHeight]).children()[currentWidth]);
     }
 
     function lockTable() {
@@ -28,23 +52,41 @@ $(document).ready(function() {
         $($(tableStruct)[0]).css('pointer-events', 'auto');
     }
 
+    $("#reset").click(function(e) {
+        if (e.preventDefault()) {
+            e.preventDefault();
+        }
+        if (isPinned) {
+            var cell = getCellElement(pinnedHeightLevel, pinnedWeightLevel);
+            $($($(cell)[0]).children()[0]).text('.');
+            $($($(cell)[0]).children()[1]).text('2');
+        }
+        resetWord();
+        $($(".current_word")[0]).val(currentWordStr);
+    });
+
+
+
     $(".cell").click(function(e) {
         if (e.preventDefault()) {
             e.preventDefault();
         }
-        if ($($($(this).parent()).children()[1]).text() != '2') {
-            return;
-        }
-        if (isPinned) {
-            return;
-        }
         var parent = $($(this).parent())[0];
         var parentParent = $($(parent).parent())[0];
-        pinnedHeightLevel = $($(parentParent).children()).index(parent);
-        pinnedWeightLevel = $($($($(parentParent).parent())[0]).children()).index(parentParent);
-        isPinned = true;
-        $("#letters_panel").css('display', 'block');
-        lockTable();
+        if (isPinned) {
+            var indexWeightLevel = $($(parentParent).children()).index(parent);
+            var indexHeightLevel = $($($($(parentParent).parent())[0]).children()).index(parentParent);
+            addLetter(indexHeightLevel, indexWeightLevel, $(this).text());
+        } else {
+            if ($($($(this).parent()).children()[1]).text() != '2') {
+                return;
+            }
+            pinnedWeightLevel = $($(parentParent).children()).index(parent);
+            pinnedHeightLevel = $($($($(parentParent).parent())[0]).children()).index(parentParent);
+            isPinned = true;
+            $("#letters_panel").css('display', 'block');
+            lockTable();
+        }
     });
 
     $(".letter_button").click(function(e) {
@@ -56,13 +98,14 @@ $(document).ready(function() {
             return;
         }
 
-        var cell = getCellElement(pinnedWeightLevel, pinnedHeightLevel);
+        var cell = getCellElement(pinnedHeightLevel, pinnedWeightLevel);
 
         $($($(cell)[0]).children()[0]).text($(this).text());
         $($($(cell)[0]).children()[1]).text('1');
 
         $($($(this).parent())[0]).css('display', 'none');
         unlockTable();
+        isPinned = true;
     });
 
 
@@ -103,10 +146,6 @@ $(document).ready(function() {
     }
 
 
-
-
-
-
     onWait();
-    setInterval(onWait, 100000);
+    setInterval(onWait, 10000);
 });
